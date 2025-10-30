cbfae65548e6591d8ef58b3ea14034f38a7174e6
useivy <0.smvl@proton.me>
2025-10-30 19:34:20 +0100

    2025-10-30 19:34 fix 2

--- boot.asm
+++ boot.asm
@@ -1,18 +1,8 @@
 [org 0x7C00]
 [bits 16]
 
-xor ax, ax
-xor bx, bx
-xor cx, cx
-xor dx, dx
-xor bp, bp
-xor sp, sp
-xor cs, cs
-xor ds, ds
-xor ss, ss
-
 mov bx, MSG_FIRST
-call print_bios
+call bios_print
 
 jmp $
 
